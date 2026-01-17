90a5dc0228fd970ec395c03f22ded5b8091a94c8
corielljacob <32170426+corielljacob@users.noreply.github.com>
2026-01-17 13:03:53 -0600

    Fix staff

--- MogTomeApi/Data/FreeCompanyStaffMember.cs
+++ MogTomeApi/Data/FreeCompanyStaffMember.cs
@@ -5,7 +5,6 @@ namespace MogTomeApi.Data
     [BsonIgnoreExtraElements]
     public class FreeCompanyStaffMember : FreeCompanyMember
     {
-        public string Biography { get; set; }
         public DateTime? PromotionDate { get; set; }
         public bool RecentlyPromoted { get { return PromotionDate != null && PromotionDate >= DateTime.Now.AddDays(-30); } }
     }
